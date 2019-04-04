2d56b93cf268bfae570c20ffbef0b79799ad9f7c
iottech <iottech>
2019-04-04 15:51:53 +0530

    comments

--- Place_Order.py
+++ Place_Order.py
@@ -43,32 +43,10 @@ def Query_today_food_orders(request):
        for category in category_type:
            all_category.append({'category':category,'items':[tab_order for tab_order in all_table_orders if tab_order['category']==category]})
        order_no = all_table_orders[0]['order_no']
-       total_orders.append({'table_no':table,'order_no':order_no,'all_items':all_category})
+       comments = json.loads(dbget("select * from public.order_comments where order_no='"+order_no+"' order by comments_time "))
+       total_orders.append({'table_no':table,'order_no':order_no,'all_items':all_category,'commenst':comments})
    ed_time = time.time()
    full_time = ed_time - st_time
    print("Time Taken", full_time)
    return json.dumps({"Returnvalue":total_orders},indent=2)
 
-def Query_food_orders(request):
-    print("daw")
-    food_order_details,food_details,categories,list1 = [],{},[],[]
-    get_orders=json.loads(dbget("select order_status.order_status_desc,food_menu.food_name,food_menu.price,food_category.category, food_order.* from food_order\
-	left join food_menu on food_menu.food_id = food_order.food_id \
-	left join food_category on food_category.category_id =food_menu.item_category_id \
-	left join order_status on order_status.order_status_id = food_order.order_status_id order by datetime"))
-    for get_order in get_orders:
-          value = str(get_order['table_no'])
-          print(value)
-          if value not in food_details.keys():
-            
-                 food_details[""+str(get_order['table_no'])+""] =[]
-                 food_order_details.append({"tableno":get_order['table_no'],"category_list":[]})
-          else:
-              pass
-          
-          food_details[""+str(get_order['table_no'])+""].append(get_order['category'])
-              
-    print(food_details)
-                
-                     # if get_order['category'] == category['category_name']:
-    return json.dumps({"Returnvalue":food_order_details},indent=2)
