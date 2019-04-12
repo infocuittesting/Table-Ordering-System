00609e214f8a1a439946bed346ada8dbc55d5eb5
iottech <iottech>
2019-04-12 11:23:58 +0530

    place order service

--- Place_Order.py
+++ Place_Order.py
@@ -5,9 +5,9 @@ import time
 def Place_Order(request):
     
     d = request.json
-    print(d)
+    #print(d)
     items_value = d['items']
-    unavailable_item = []
+    list1 = []
     check_item = tuple(''+str(item_va['food_id'])+'' for item_va in items_value)
     
     get_table_status = json.loads(dbget("select table_status_id from table_details where table_no="+str(d['table_no'])+""))
@@ -22,20 +22,23 @@ def Place_Order(request):
             print(order_no)
             
             for item in items_value:
-                item.update({'order_no':order_no,'notification_status_id':3,'datetime':application_datetime(),'table_no':d['table_no']})
-                get_items = json.loads(dbget("select food_status_id,food_name from food_menu where food_id = '"+str(item['food_id'])+"' "))
-                if get_items[0]['food_status_id'] == 1:
-                   gensql('insert','food_order',item)
-                else:
-                    unavailable_item.append({"food_name":get_items[0]['food_name'],"food_id":item['food_id']})
-                        
+                list1.append(tuple((order_no,d['table_no'],item['food_id'],item['quantity'],item['order_status_id'],str(application_datetime().strftime("%Y-%m-%d %H:%M:%S")),3)))
+                #item.update({'order_no':order_no,'notification_status_id':3,'datetime':application_datetime(),'table_no':d['table_no']})
+                #get_items = json.loads(dbget("select food_status_id,food_name from food_menu where food_id = '"+str(item['food_id'])+"' "))
+                #if get_items[0]['food_status_id'] == 1:
+                 #  gensql('insert','food_order',item)
+                #else:
+                 #   unavailable_item.append({"food_name":get_items[0]['food_name'],"food_id":item['food_id']})
+            values = ', '.join(map(str, list1))
+            dbput("INSERT INTO  food_order (order_no, table_no, food_id, quantity, order_status_id, datetime, notification_status_id)VALUES {}".format(values))
+                   
             try:
                 maintain_time = {"order_no":order_no,"order_time":application_datetime()}
                 gensql('insert','order_timings',maintain_time)
             except:
                 pass
-            if d['comments'] is not None or d['comments'] != '':
-                order_comments = {'order_no':order_no,'order_comments':d['comments'],'comments_time':item['datetime']}
+            if d['comments'] is not None:
+                order_comments = {'order_no':order_no,'order_comments':d['comments'],'comments_time':application_datetime()}
                
                 gensql('insert','order_comments',order_comments)
             else:
@@ -51,11 +54,13 @@ def Place_Order(request):
 def Query_today_food_orders(request):
    st_time = time.time()
    today_orders = json.loads(dbget("select food_menu.food_name,food_category.*, food_order.*,\
-                                   order_status.order_status_desc from food_order\
-                                    left join food_menu on food_menu.food_id = food_order.food_id\
-                                    left join food_category on food_category.category_id =food_menu.item_category_id\
-                                    left join order_status on order_status.order_status_id = \
-                                    food_order.order_status_id where food_order.order_status_id!=7 \
+                                  order_status.order_status_desc,notification_status.* from food_order\
+                                   left join food_menu on food_menu.food_id = food_order.food_id\
+                                   left join food_category on food_category.category_id =food_menu.item_category_id\
+                                   left join order_status on order_status.order_status_id = \
+                                   food_order.order_status_id join notification_status on\
+                                   food_order.notification_status_id = notification_status.notification_status_id \
+                                   where food_order.order_status_id!=7 \
                                    order by datetime"))
    #print(today_orders)
    tables = list(set(order['table_no'] for order in today_orders))
